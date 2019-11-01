29a3dd1c5829cdb679cbffe0f667eca54169b009
zen <zennyosh@gmail.com>
2019-11-01 13:23:52 +0900

    add sample code

---
+++ README.md
@@ -0,0 +1,9 @@
+# go サンプル
+## 実行
+    $ go run hello.go
+
+## コンパイル
+    $ go build hello.go
+
+## Windows用バイナリのクロスコンパイル
+    $ GOOS=windows GOARCH=amd64 go build hello.go
---
+++ hello.go
@@ -0,0 +1,11 @@
+// パッケージ名を指定 --- (*1)
+package main
+
+// fmt パッケージを取り込む --- (*2)
+import "fmt"
+
+// main関数の定義 --- (*3)
+func main() {
+    // 文字列を出力する --- (*4)
+    fmt.Printf("hello, world!\n")
+}
